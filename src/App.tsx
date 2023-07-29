cd6f2e0b97a808aac4850e4a49071bb384747bbd
Vladimirov Daniil <seventimezzz@gmail.com>
2023-07-29 17:24:59 +0300

    websocket

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import { EventSourcing } from './EventSourceing';
 import { LongPulling } from './LongPulling';
+import  WebSock  from './WebSock';
 
 
 function App() {
   return (
     <div className="App">
       {/* <LongPulling></LongPulling> */}
-      <EventSourcing/>
+      {/* <EventSourcing/> */}
+        <WebSock></WebSock>
     </div>
   );
 }
